9be4b7f6f297d0071329c21e62efa8f5572e937c
Kani <gkounkoud@myges.fr>
2024-06-10 21:34:35 +0200

    Update NavBarAdmin component to adjust the width of the collapsed state from "w-14" to "w-15" for better visual alignment. Also, add dynamic highlighting of the current page by comparing the currentPath with the href value. Finally, update the hover background color to "primary/20" for improved user experience.

--- front/src/components/admin/NavItem.tsx
+++ front/src/components/admin/NavItem.tsx
@@ -7,40 +7,49 @@ import {
   TooltipContent,
   TooltipProvider,
 } from "../ui/tooltip";
+import { usePathname } from "next/navigation";
 
 interface NavItemProps {
-  icon: React.ElementType; // Définissez le type de l'icône comme un composant React
+  icon: React.ElementType;
   label: string;
   href: string;
   isCollapsed: boolean;
 }
 
-const NavItem: React.FC<NavItemProps> = ({
+function NavItem({
   icon: Icon,
   label,
   href,
   isCollapsed,
-}) => (
-  <li>
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link
-            href={href}
-            className={clsx("flex items-center p-2 rounded hover:bg-gray-100", {
-              "justify-center": isCollapsed,
-              "justify-start": !isCollapsed,
-            })}
-          >
-            <Icon className={clsx("h-6 w-6", { "mr-2": !isCollapsed })} />{" "}
-            {/* Utilisez le composant Icon */}
-            {!isCollapsed && <span>{label}</span>}
-          </Link>
-        </TooltipTrigger>
-        {isCollapsed && <TooltipContent side="right">{label}</TooltipContent>}
-      </Tooltip>
-    </TooltipProvider>
-  </li>
-);
+}: NavItemProps): JSX.Element {
+  const currentPath = usePathname();
+  return (
+    <li>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href={href}
+              className={clsx(
+                "flex items-center p-2 rounded hover:bg-primary/20",
+                {
+                  "justify-center": isCollapsed,
+                  "justify-start": !isCollapsed,
+                },
+                {
+                  "text-primary": currentPath === href,
+                }
+              )}
+            >
+              <Icon className={clsx("h-8 w-6", { "mr-2": !isCollapsed })} />{" "}
+              {!isCollapsed && <span>{label}</span>}
+            </Link>
+          </TooltipTrigger>
+          {isCollapsed && <TooltipContent side="right">{label}</TooltipContent>}
+        </Tooltip>
+      </TooltipProvider>
+    </li>
+  );
+}
 
 export default NavItem;
