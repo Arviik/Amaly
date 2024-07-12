cb7a4503401dffedd5fbaa40bbd06b8fffc0769d
Kani <gkounkoud@myges.fr>
2024-07-12 22:18:40 +0200

    refactor: Update login page to redirect based on user role after successful login

--- front/src/app/(protected)/profiles/page.tsx
+++ front/src/app/(protected)/profiles/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -13,26 +12,19 @@ export default function SelectOrganization() {
   const router = useRouter();
   const dispatch = useDispatch();
   const memberships = useSelector(selectMemberships);
-  const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  const handleSelectOrganization = (orgId: number, isAdmin: boolean) => {
+  const handleSelectOrganization = (orgId: number) => {
     dispatch(setSelectedOrganization(orgId));
-    // Utilisez setTimeout pour s'assurer que la navigation se produit après la mise à jour du state
-    setTimeout(() => {
-      router.push(isAdmin ? "/dashboard" : "/membres"); //regler le soucis ici
-    }, 0);
+    const selectedMembership = memberships.find(
+      (m) => m.organizationId === orgId
+    );
+    if (selectedMembership) {
+      router.push(selectedMembership.isAdmin ? "/dashboard" : "/member");
+    }
   };
 
-  if (!isClient) {
-    return null; // ou un loader si vous préférez
-  }
-
   return (
-    <div className="container mx-auto mt-10">
+    <div className=" mx-auto mt-10">
       <h1 className="text-2xl font-bold mb-6">Sélectionnez une organisation</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {memberships.map((membership) => (
@@ -43,10 +35,7 @@ export default function SelectOrganization() {
             <CardContent>
               <Button
                 onClick={() =>
-                  handleSelectOrganization(
-                    membership.organizationId,
-                    membership.isAdmin
-                  )
+                  handleSelectOrganization(membership.organizationId)
                 }
                 className="w-full"
               >
