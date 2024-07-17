d4168fe19ec77a3d1fed0008c9b76d96e4142540
Kyllian Maignan <shokkzu@gmail.com>
2024-07-17 16:47:00 +0200

    refractor: Changed Return in chatbot.ts

--- front/src/api/services/chatbot.ts
+++ front/src/api/services/chatbot.ts
@@ -6,13 +6,12 @@ export interface startThreadResponse{
 }
 
 export const startThread = async (message: string, organizationId: number): Promise<startThreadResponse> => {
-    const response: startThreadResponse = await api.post('chatbot/open', {
+    return await api.post('chatbot/open', {
         json: {
             message: message,
             organizationId: organizationId,
         }
     }).json();
-    return response;
 }
 
 export interface continueThreadResponse{
@@ -21,11 +20,10 @@ export interface continueThreadResponse{
 }
 
 export const continueThread = async (message: string, threadId: string): Promise<startThreadResponse>  => {
-    const response: startThreadResponse = await api.post('chatbot/continue', {
+    return await api.post('chatbot/continue', {
         json: {
             message: message,
             threadId: threadId,
         }
     }).json();
-    return response;
 }
