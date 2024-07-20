8ac0c7dc961e9b066506c777da266e77e3fa02be
Kani <gkounkoud@myges.fr>
2024-07-20 23:27:28 +0200

    refactor: Update donationsValidation and donationsPatchValidation in donation-validator.ts

--- api/src/api/validators/donation-validator.ts
+++ api/src/api/validators/donation-validator.ts
@@ -1,25 +1,25 @@
 import Joi from "joi";
 
 export interface donationsRequest {
-    amount: number;
-    date: Date;
-    donorName: string;
-    donorEmail: string;
-    organizationId: number;
+  amount: number;
+  date: Date;
+  donorName: string;
+  donorEmail: string;
+  organizationId: number;
 }
 
 export const donationsValidation = Joi.object<donationsRequest>({
-    amount: Joi.number().required(),
-    date: Joi.date().required(),
-    donorName: Joi.string().required(),
-    donorEmail: Joi.string().email().required(),
-    organizationId: Joi.number().required(),
+  amount: Joi.number().required(),
+  date: Joi.date().required(),
+  donorName: Joi.string().required(),
+  donorEmail: Joi.string().email().required(),
+  organizationId: Joi.number().required(),
 }).options({ abortEarly: true });
 
 export const donationsPatchValidation = Joi.object<donationsRequest>({
-    amount: Joi.number().required(),
-    date: Joi.date().required(),
-    donorName: Joi.string().required(),
-    donorEmail: Joi.string().email().required(),
-    organizationId: Joi.number().required(),
+  amount: Joi.number().required(),
+  date: Joi.date().required(),
+  donorName: Joi.string().required(),
+  donorEmail: Joi.string().email().required(),
+  organizationId: Joi.number().required(),
 }).options({ abortEarly: true });
