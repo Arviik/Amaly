6dcd0d912a29f9479fb38362f84263d81b20e1fe
Kani <gkounkoud@myges.fr>
2024-07-23 21:17:56 +0200

    refactor: Update routes.ts to include new routes for stripes, subscriptions, and membership types

--- api/src/api/routes/organizations.ts
+++ api/src/api/routes/organizations.ts
@@ -3,26 +3,25 @@ import { prisma } from "../../utils/prisma";
 import { authMiddleware } from "../middlewares/auth-middleware";
 import { authzMiddleware } from "../middlewares/authz-middleware";
 import {
-    organizationPatchValidation,
-    organizationValidation,
+  organizationPatchValidation,
+  organizationValidation,
 } from "../validators/organization-validator";
-import {v4} from "uuid"
+import { v4 } from "uuid";
 
 export const initOrganizations = (app: express.Express) => {
-    app.get(
-        "/organizations",
-        authMiddleware,
-        authzMiddleware,
-        async (req, res) => {
-            try {
-                const allOrganizations = await prisma.organizations.findMany();
-                res.json(allOrganizations);
-            } catch (e) {
-                res.status(500).send({error: e});
-                return;
-            }
-        }
-    );
+  app.get(
+    "/organizations",
+
+    async (req, res) => {
+      try {
+        const allOrganizations = await prisma.organizations.findMany();
+        res.json(allOrganizations);
+      } catch (e) {
+        res.status(500).send({ error: e });
+        return;
+      }
+    }
+  );
 
   app.get("/organizations/:id(\\d+)", async (req, res) => {
     try {
@@ -110,93 +109,89 @@ export const initOrganizations = (app: express.Express) => {
     }
   );
 
-    app.get("/organizations/createinvite/:id", async (req, res) => {
-        try {
-            if (!req.params.id){
-                res.status(400).send({error: "You must specify a id"});
-                return;
-            }
-            const uuid = v4();
-            const alreadyExists = await prisma.organizationInvitation.findUnique({
-                where: {organizationId: Number(req.params.id)},
-            })
-            let invitation;
-            if (alreadyExists){
-                invitation = await prisma.organizationInvitation.update({
-                    where: {organizationId: Number(req.params.id)}
-                    ,data: {
-                        uuid: uuid,
-                    }
-                });
-            }else{
-                invitation = await prisma.organizationInvitation.create({
-                    data: {
-                        organizationId: Number(req.params.id),
-                        uuid: uuid,
-                    }
-                });
-            }
-            res.json(invitation);
-        } catch (e) {
-            res.status(500).send({ error: e });
-        }
-    });
-
-    app.get("/organizations/invite", async (req, res) => {
-        try {
-            if (!req.query.uuid) {
-                res.status(400).send({error: "You must specify a uuid"});
-                return;
-            }
-            const invitation = await prisma.organizationInvitation.findFirst({
-                where: {uuid: String(req.query.uuid)},
-                include: {
-                    organization: true
-                }
-            });
-            res.json(invitation);
-        } catch (e) {
-            res.status(500).send({error: e});
-        }
-    });
-
-    app.post(
-        "/organizations",
-        authMiddleware,
-        async (req, res) => {
-            const validation = organizationValidation.validate(req.body);
-
-            if (validation.error) {
-                res.status(400).send({error: validation.error});
-                return;
-            }
-
-      const organizationRequest = validation.value;
-      try {
-        const organization = await prisma.organizations.create({
-          data: {
-            name: organizationRequest.name,
-            type: organizationRequest.type,
-            address: organizationRequest.address,
-            email: organizationRequest.email,
-            phone: organizationRequest.phone,
-            ownerId: organizationRequest.ownerId,
-          },
-        });
-        res.json(organization);
-      } catch (e) {
-        res.status(500).send({ error: e });
+  app.get("/organizations/createinvite/:id", async (req, res) => {
+    try {
+      if (!req.params.id) {
+        res.status(400).send({ error: "You must specify a id" });
         return;
       }
+      const uuid = v4();
+      const alreadyExists = await prisma.organizationInvitation.findUnique({
+        where: { organizationId: Number(req.params.id) },
+      });
+      let invitation;
+      if (alreadyExists) {
+        invitation = await prisma.organizationInvitation.update({
+          where: { organizationId: Number(req.params.id) },
+          data: {
+            uuid: uuid,
+          },
+        });
+      } else {
+        invitation = await prisma.organizationInvitation.create({
+          data: {
+            organizationId: Number(req.params.id),
+            uuid: uuid,
+          },
+        });
+      }
+      res.json(invitation);
+    } catch (e) {
+      res.status(500).send({ error: e });
     }
-  );
+  });
 
-    app.patch(
-        "/organizations/:id",
-        authMiddleware,
-        authzMiddleware,
-        async (req, res) => {
-            const validation = organizationPatchValidation.validate(req.body);
+  app.get("/organizations/invite", async (req, res) => {
+    try {
+      if (!req.query.uuid) {
+        res.status(400).send({ error: "You must specify a uuid" });
+        return;
+      }
+      const invitation = await prisma.organizationInvitation.findFirst({
+        where: { uuid: String(req.query.uuid) },
+        include: {
+          organization: true,
+        },
+      });
+      res.json(invitation);
+    } catch (e) {
+      res.status(500).send({ error: e });
+    }
+  });
+
+  app.post("/organizations", authMiddleware, async (req, res) => {
+    const validation = organizationValidation.validate(req.body);
+
+    if (validation.error) {
+      res.status(400).send({ error: validation.error });
+      return;
+    }
+
+    const organizationRequest = validation.value;
+    try {
+      const organization = await prisma.organizations.create({
+        data: {
+          name: organizationRequest.name,
+          type: organizationRequest.type,
+          address: organizationRequest.address,
+          email: organizationRequest.email,
+          phone: organizationRequest.phone,
+          ownerId: organizationRequest.ownerId,
+        },
+      });
+      res.json(organization);
+    } catch (e) {
+      res.status(500).send({ error: e });
+      return;
+    }
+  });
+
+  app.patch(
+    "/organizations/:id",
+    authMiddleware,
+    authzMiddleware,
+    async (req, res) => {
+      const validation = organizationPatchValidation.validate(req.body);
 
       if (validation.error) {
         console.log("error validation", validation.error);
@@ -223,25 +218,25 @@ export const initOrganizations = (app: express.Express) => {
     }
   );
 
-    app.delete(
-        "/organizations/:id",
-        authMiddleware,
-        authzMiddleware,
-        async (req, res) => {
-            try {
-                const deletedOrganization = await prisma.organizations.delete({
-                    where: {id: Number(req.params.id)},
-                });
-                res.status(200).json(deletedOrganization);
-            } catch (e) {
-                res.status(500).send({error: e});
-            }
-        }
-    );
+  app.delete(
+    "/organizations/:id",
+    authMiddleware,
+    authzMiddleware,
+    async (req, res) => {
+      try {
+        const deletedOrganization = await prisma.organizations.delete({
+          where: { id: Number(req.params.id) },
+        });
+        res.status(200).json(deletedOrganization);
+      } catch (e) {
+        res.status(500).send({ error: e });
+      }
+    }
+  );
 
-    app.post(
-        "/organization/:organizationId/join",
-        authMiddleware,
-        async (req, res) => {}
-    );
+  app.post(
+    "/organization/:organizationId/join",
+    authMiddleware,
+    async (req, res) => {}
+  );
 };
